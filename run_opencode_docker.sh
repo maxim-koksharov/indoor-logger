cb94663b45bb9302b4c6ebba05418584f46d281c
Maxim Koksharov <maxim_koksharov@protonmail.com>
2026-07-08 17:42:03 +0100

    Delete unused shell scripts

--- run_opencode_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it \
-  --device=/dev/ttyUSB0:/dev/ttyUSB0 \
-  -v "$PWD":/esp/project \
-  -v ~/.local/share/opencode:/root/.local/share/opencode \
-  -v ~/.config/opencode:/root/.config/opencode \
-  esp8266-env \
-  opencode
